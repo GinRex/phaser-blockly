df75e46bc0fdf5b4c74ea40174614b59b918db44
MAC <mac@MACs-MacBook-Pro.local>
2019-03-19 20:22:17 +0700

    add auto create game folder by name

--- src/client/Scenes/main.jsx
+++ src/client/Scenes/main.jsx
@@ -5,101 +5,92 @@ import Ghost from './Ghost';
 // import { connect } from "react-redux";
 
 class MainScene extends Phaser.Scene {
-    
-    constructor(props) {
-        super(props)
-        store.subscribe(this.restartGame)
-    }
+  constructor(props) {
+    super(props);
+    store.subscribe(this.restartGame);
+  }
 
-    restartGame = () => {
-        console.log(store.getState());
-        this.scene.restart();
-    }
+  restartGame = () => {
+    console.log(store.getState());
+    this.scene.restart();
+  };
 
+  MainScene() {
+    Phaser.Scene.call(this, { key: 'main', active: false });
+  }
 
-    MainScene() {
-        Phaser.Scene.call(this, { key: 'main', active: false });
-    }
+  preload() {
+    this.load.image('hero', 'assets/Hero.gif');
+    this.load.image('ghost', 'assets/ghost.png');
+  }
 
-    preload() {
-        this.load.image('hero', 'assets/Hero.gif');
-        this.load.image('ghost', 'assets/ghost.png')
-    }
+  create() {
+    // if (store.getState().gameObjects.length !== 0) {
+    //     console.log('abc')
+    //     // console.log(store.getState())
+    //     var gameObjects = store.getState().gameObjects;
+    //     gameObjects.forEach(object => {
+    //         eval(object.jsCode);
+    //     });
 
-    create() {
+    // }
 
-        // if (store.getState().gameObjects.length !== 0) { 
-        //     console.log('abc')
-        //     // console.log(store.getState())
-        //     var gameObjects = store.getState().gameObjects;
-        //     gameObjects.forEach(object => {
-        //         eval(object.jsCode);
-        //     });
-            
-        // }
+    this.Hero = new Hero({
+      scene: this,
+      key: 'hero',
+      x: 400,
+      y: 400,
+      width: 100,
+      height: 100,
+    });
 
-        this.Hero = new Hero({
-            scene: this,
-            key: 'hero',
-            x: 400,
-            y: 400,
-            width: 100,
-            height: 100
-        })
+    this.Ghost = new Ghost({
+      scene: this,
+      key: 'ghost',
+      x: 200,
+      y: 200,
+      width: 100,
+      height: 100,
+    });
+    // this.Hero = this.add.sprite(400, 400, 'picA').setDisplaySize(100, 100).setInteractive();
+    // this.Ghost = this.add.sprite(300, 200, 'picB').setDisplaySize(100, 100).setInteractive();
 
-        this.Ghost = new Ghost({
-            scene: this,
-            key: 'ghost',
-            x: 200,
-            y: 200,
-            width: 100,
-            height: 100
-        })
-        // this.Hero = this.add.sprite(400, 400, 'picA').setDisplaySize(100, 100).setInteractive();
-        // this.Ghost = this.add.sprite(300, 200, 'picB').setDisplaySize(100, 100).setInteractive();
+    // this.input.setDraggable(this.Hero);
+    // this.input.setDraggable(this.Ghost);
 
+    // this.input.on('dragstart', function (pointer, gameObject) {
 
-        // this.input.setDraggable(this.Hero);
-        // this.input.setDraggable(this.Ghost);
+    //     gameObject.setTint(0xff0000);
 
+    // });
 
-        // this.input.on('dragstart', function (pointer, gameObject) {
+    // this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
 
-        //     gameObject.setTint(0xff0000);
+    //     gameObject.x = dragX;
+    //     gameObject.y = dragY;
 
-        // });
+    // });
 
-        // this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
+    // this.input.on('dragend', function (pointer, gameObject) {
 
-        //     gameObject.x = dragX;
-        //     gameObject.y = dragY;
+    //     gameObject.clearTint();
 
-        // });
+    // });
+  }
 
-        // this.input.on('dragend', function (pointer, gameObject) {
+  update() {}
 
-        //     gameObject.clearTint();
+  // set time out
+  // set theo event
 
-        // });
+  tint() {
+    alert('asdasd');
+    console.log('asdasd');
+  }
 
-    }
+  clearTint() {
+    this.clearTint();
+  }
+}
 
-    update() {
-    
-    }
-
-    //set time out
-    // set theo event 
-
-    tint() {
-        alert('asdasd')
-        console.log('asdasd')
-    }
-
-    clearTint() {
-        this.clearTint();
-    }
-
-};
-
-export default MainScene
+export default MainScene;
---
+++ src/server/gameTemplate.js
@@ -0,0 +1,30 @@
+import Phaser from 'phaser';
+import MainScene from './Scenes/Main';
+import * as React from 'react';
+
+export default class Game extends React.Component {
+  componentDidMount() {
+    const config = {
+      type: Phaser.AUTO,
+      pixelArt: true,
+      width: 500,
+      height: 400,
+      parent: 'phaser-game',
+      physics: {
+        default: 'arcade',
+        arcade: {
+          gravity: {
+            y: 800,
+          },
+          debug: false,
+        },
+      },
+      scene: [MainScene],
+    };
+    const game = new Phaser.Game(config);
+  }
+
+  render() {
+    return <div id="phaser-game" />;
+  }
+}
