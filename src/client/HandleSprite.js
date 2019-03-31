7ff781fb37818eb1510e7258a9f6412d2cea5111
duy <duongtrungduyit@gmail.com>
2019-03-31 23:15:47 +0700

    update

---
+++ src/client/HandleSprite.js
@@ -0,0 +1,11 @@
+function hide_current_sprite(game_object){
+    game_object.visible = false;
+}
+
+function show_current_sprite(game_object){
+    game_object.visible = true;
+}	
+
+export {
+    hide_current_sprite, show_current_sprite, 
+}
--- src/client/toolBox.jsx
+++ src/client/toolBox.jsx
@@ -10,13 +10,13 @@ export default [
       { type: 'motion_point_in_direction_of_target' },
       { type: 'motion_point_in_direction_of_point' },
       { type: 'motion_set_x_to' },
-      { type: 'motion_set_y_to' },
-    ],
+      { type: 'motion_set_y_to' }
+    ]
   },
   {
     name: 'Event',
     colour: 250,
-    blocks: [{ type: 'events_when_event_happens' }],
+    blocks: [{ type: 'events_when_event_happens' }]
   },
   {
     name: 'Sprites',
@@ -28,6 +28,18 @@ export default [
       { type: 'sprites_show_current_sprite' },
       { type: 'sprites_for_each_clone_of_current_sprite' },
       { type: 'sprites_is_colliding_with_target' },
-    ],
-  },
-];
+      // { type: 'sprites_set_current_sprite'},
+      // { type: 'sprites_set_target_sprite'},
+      { type: 'sprites_value_of_x' },
+      { type: 'sprites_value_of_y' },
+      { type: 'sprites_nested_for_each_clone_of_current_target_sprite' },
+      { type: 'sprites_for_each_clone_of_current_sprite_new' },
+      { type: 'sprites_next_frame' },
+      { type: 'sprites_current_sprite_send_to_back' },
+      { type: 'sprites_current_sprite_bring_to_front' },
+      { type: 'sprites_set_current_sprite_to_target_sprite' },
+      { type: 'sprites_current_frame_number' },
+      { type: 'sprites_set_frame_to_frame_number' }
+    ]
+  }
+]
